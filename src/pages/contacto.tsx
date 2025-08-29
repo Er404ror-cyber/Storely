6844390c12d7354a57eb09d324eca43abb6c804f
ddt <dicksontembe4420@gmail.com>
2025-08-29 20:18:32 +0200

    primeiro esboco

--- src/pages/contacto.tsx
+++ src/pages/contacto.tsx
@@ -6,7 +6,42 @@ export const Contacto = () =>{
         <div>
             <Header/>
 
-            pagina de contacots.....
+            <div className="flex justify-between">    
+                <div className="w-1/2 flex justify-center mt-32 ">
+
+                <form action="">
+                    <h2>Nome:</h2>
+                    <label htmlFor="nome" className="text-gray-700 font-semibold"></label>
+                    <input type="text" required placeholder="Digite seu nome" 
+                    className="border-gray-300 border hover:bg-black/5 transition-all duration-300 ease-in-out p-2 
+                    rounded mt-1" />
+
+                    <h2>e-mail:</h2>
+                    <label htmlFor="e-mail" className="text-gray-700 font-semibold"></label>
+                    <input type="email"  required placeholder="Digite seu e-mail" 
+                    className="border-gray-300 border hover:bg-black/5 transition-all duration-300 ease-in-out p-2 
+                    rounded mt-1" />
+
+                     <h2>Deixe o seu pedido:</h2>
+                    <label htmlFor="mensagem" className="text-gray-700 font-semibold"></label>
+                    <input type="text"  required placeholder="Deixe seu pedido" 
+                    className="border-gray-300 border hover:bg-black/5 transition-all duration-300 ease-in-out p-2 
+                    rounded mt-1" />
+
+                    <button type="submit" 
+                    className="border border-black  hover: bg-blue-500 transition-all duration-300
+                    ease-in-out p-2 rounded grid mt-6 ml-12">Submeter</button>
+
+                </form>
+
+                </div>
+
+                <div className="w-1/2 h-screen bg-gradient-to-r from-pink-300 to-red-400">
+                <img src="src/img/casal.JPEG" alt="foto"
+                className="h-120 items-center" />
+                    
+                </div>`
+            </div>
         </div>
     )
 }
--- src/pages/rosas.tsx
+++ src/pages/rosas.tsx
@@ -1,13 +1,152 @@
-import { Header } from "../components/header"
+import { Header } from '../components/header';
 
+const Rosas = () => {
+  return (
 
-export const Rosas = () =>{
-    return(
-        <div>
-            <Header/>
-            
-            pagina de conteudo
+  <div className='h-screen bg-gray-200'> 
+    <Header/>
+    <div className="flex items-center justify-center  p-6 ">
+      <div className="flex max-w-2xl w-full rounded-lg shadow-lg bg-white  m-2">
+       
+        <div className="w-2/6 p-2 bg-sky-50">
+          <img 
+            src="src/img/2_caixs.JPEG"
+            alt="Foto "
+            className="w-full h-full object-cover rounded-md"
+          />
+          
         </div>
-    )
+
+        
+        <div className="w-4/6 p-6">
+          <h3 className="text-xl font-semibold mb-2">Flower box</h3>
+          <p className="text-gray-600 mb-3 text-sm">
+            rosas de caixa rosas de caixa rosas de caixa rosas de caixa rosas de caixa rosas de caixa
+            rosas de caixa rosas de caixa v rosas de caixa
+          </p>
+
+        </div>
+        
+      </div>
+
+        <div className="flex max-w-2xl w-full rounded-lg shadow-lg bg-white  m-2">
+       
+        <div className="w-2/6 p-2 bg-sky-50">
+          <img 
+            src="src/img/buque_20.JPEG"
+            alt="Foto "
+            className="w-full h-full object-cover rounded-md"
+          />
+          
+        </div>
+
+        
+        <div className="w-4/6 p-6">
+          <h3 className="text-xl font-semibold mb-2">Flower box</h3>
+          <p className="text-gray-600 mb-3 text-sm">
+            rosas de caixa rosas de caixa rosas de caixa rosas de caixa rosas de caixa rosas de caixa
+            rosas de caixa rosas de caixa v rosas de caixa
+          </p>
+
+        </div>
+        
+      </div>
+
+      <div className="flex max-w-2xl w-full rounded-lg shadow-lg bg-white ">
+       
+        <div className="w-2/6 p-2 bg-sky-50">
+          <img 
+            src="src/img/cetim.JPEG"
+            alt="Foto "
+            className="w-full h-full object-cover rounded-md"
+          />
+          
+        </div>
+
+        
+        <div className="w-4/6 p-6">
+          <h3 className="text-xl font-semibold mb-2">Flower box</h3>
+          <p className="text-gray-600 mb-3 text-sm">
+            rosas de caixa rosas de caixa rosas de caixa rosas de caixa rosas de caixa rosas de caixa
+            rosas de caixa rosas de caixa v rosas de caixa
+          </p>
+
+        </div>
+        
+      </div>
+
+
+    </div>
+
+    <div className="flex items-center justify-center  p-6 ">
+            <div className="flex max-w-2xl w-full rounded-lg shadow-lg bg-white m-2">
+            
+                <div className="w-2/6 p-2 bg-sky-50">
+                <img 
+                    src="src/img/1_boque.JPEG"
+                    alt="Foto "
+                    className="w-full h-full object-cover rounded-md"
+                />
+                
+                </div>
+
+                
+                <div className="w-4/6 p-6">
+                <h3 className="text-xl font-semibold mb-2">Flower box</h3>
+                <p className="text-gray-600 mb-3 text-sm">
+                    rosas de caixa rosas de caixa rosas de caixa rosas de caixa rosas de caixa rosas de caixa
+                    rosas de caixa rosas de caixa v rosas de caixa
+                </p>
+
+                </div>                            
+            </div>
+
+            <div className="flex max-w-2xl w-full rounded-lg shadow-lg bg-white m-2">
+            
+                <div className="w-2/6 p-2 bg-sky-50">
+                <img 
+                    src="src/img/caixa_de_10.JPEG"
+                    alt="Foto "
+                    className="w-full h-full object-cover rounded-md"
+                />
+                
+                </div>
+
+                
+                <div className="w-4/6 p-6">
+                <h3 className="text-xl font-semibold mb-2">Flower box</h3>
+                <p className="text-gray-600 mb-3 text-sm">
+                    rosas de caixa rosas de caixa rosas de caixa rosas de caixa rosas de caixa rosas de caixa
+                    rosas de caixa rosas de caixa v rosas de caixa
+                </p>
+
+                </div>                            
+            </div>
+
+            <div className="flex max-w-2xl w-full rounded-lg shadow-lg bg-white m-2">
+            
+                <div className="w-2/6 p-2 bg-sky-50">
+                <img 
+                    src="src/img/mix.JPEG"
+                    alt="Foto "
+                    className="w-full h-full object-cover rounded-md"
+                />
+                
+                </div>
+
+                
+                <div className="w-4/6 p-6">
+                <h3 className="text-xl font-semibold mb-2">Flower box</h3>
+                <p className="text-gray-600 mb-3 text-sm">
+                    rosas de caixa rosas de caixa rosas de caixa rosas de caixa rosas de caixa rosas de caixa
+                    rosas de caixa rosas de caixa v rosas de caixa
+                </p>
+
+                </div>                            
+            </div>
+     </div>
+</div> 
+  )
 }
+
 export default Rosas
