dc831d3c7425dea3f33bf487a9c1ad79008411a3
Badru640 <sbsbadroo@gmail.com>
2026-01-20 23:46:47 +0530

    merge dev

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { AdminLayout } from './layout/AdminLayout';
 import Dashboard from './dashboard/dashboard';
 import { PagesList } from './dashboard/PagesList';
