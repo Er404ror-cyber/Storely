c9e4df6c6c08d4bf880d3ca90170a067014660ab
Badru640 <sbsbadroo@gmail.com>
2026-01-21 00:21:55 +0530

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // Garanta que o plugin do React está aqui
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  base: '/', // ISSO É CRUCIAL: Garante que os caminhos de arquivos sejam absolutos
+  build: {
+    outDir: 'dist',
+  }
 })
