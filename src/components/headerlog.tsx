9cf690fb012ce4bdfc49c8c5df76483521fe313a
ddt <dicksontembe4420@gmail.com>
2026-01-23 19:44:36 +0200

    home

--- src/components/headerlog.tsx
+++ src/components/headerlog.tsx
@@ -4,26 +4,37 @@ export const HeaderLog = () => {
     return (
     <header>
       {/* CONTAINER PRINCIPAL */}
-      <div className="relative flex items-center p-6 max-w-7xl mx-auto z-10 bg-purple-500/10 rounded-b-2xl ">
-        
-        {/* LOGO À ESQUERDA */}
-        <Link to="/" className="">
-          <span className="text-2xl font-bold bg-linear-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">
+      <div className='flex justify-between p-6 max-w-7xl mx-auto z-10 bg-purple-500/10 rounded-b-2xl'>
+     <div>
+      
+       {/* LOGO À ESQUERDA */}
+       <Link to="/" className="">
+          <h1 className="text-2xl font-bold bg-linear-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">
             Logo Aqui
-          </span>
+          </h1>
         </Link>
+     </div>
 
-        {/* MENU CENTRALIZADO */}
-        <nav className="absolute left-1/2 -translate-x-1/2 flex gap-8 font-medium text-lg">
+     <div> 
+     <nav className="absolute left-1/2 -translate-x-1/2 flex gap-8 font-medium text-lg">
           <Link to="" className="hover:bg-linear-to-r hover:from-blue-500 hover:to-purple-600 hover:bg-clip-text hover:text-transparent">Sobre </Link>
           <Link to="" className="hover:bg-linear-to-r hover:from-blue-500 hover:to-purple-600 hover:bg-clip-text hover:text-transparent">Duvidas</Link>          
           <Link to="" className="hover:bg-linear-to-r hover:from-blue-500 hover:to-purple-600 hover:bg-clip-text hover:text-transparent">Contatos</Link>          
-      <div className="">  
-      <Link to='/auth' className="hover:bg-linear-to-r hover:from-blue-500 hover:to-purple-600 hover:bg-clip-text hover:text-transparent
-                                  bg-white border rounded-2xl  p-2  ">Login</Link> </div>         
           </nav>
+     </div>
 
-      </div>
+     <div>
+     <Link to='/auth' 
+      className="bg-white text-gray-800 p-2 rounded-2xl font-medium ">Login</Link> 
+            
+     </div>
+  </div>
+      {/* CONTAINER PRINCIPAL */}
+
+
+        
+     
+
     </header>
   );
 }
