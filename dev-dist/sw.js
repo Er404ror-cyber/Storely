4a8a3a84e7e52e47b66182f7b5254a94e6e5f0f8
ddt <dicksontembe4420@gmail.com>
2026-04-20 19:53:03 +0200

    verificacao

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.du59l8ctsk"
+    "revision": "0.qitsvopoh3k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
