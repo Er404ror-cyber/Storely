5f05a1a72f8dba511c4f778f79c622898c9acaa0
Badru640 <sbsbadroo@gmail.com>
2026-07-29 21:25:22 +0530

    update: gallery LTS

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f2viil3q8n"
+    "revision": "0.t08amt4vmcg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
